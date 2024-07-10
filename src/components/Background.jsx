221ab82f35ea786849e275fd2c365917323d442f
Ariels875 <ascastro875@gmail.com>
2024-07-10 04:11:43 -0500

    Added starry sky background

---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import '../styles/Background.css';
+
+const Background = () => {
+  return (
+    <div className="stars-container">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+    </div>
+  );
+};
+
+export default Background;
